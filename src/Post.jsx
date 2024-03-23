eba82a72112988dce86a0c66ed69eb067b8484ec
choplua <chrischris1176@gmail.com>
2024-03-22 22:54:59 -0400

    click button

--- src/Post.jsx
+++ src/Post.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import Comment from './Comment';
 function Post(){
+    function handleLike(){
+        console.log('Liked!');
+    }
     return(
         <div>
             <h3>Test Post</h3>
             <p>This is a test post we'll edit later!</p>
             <p>Likes: 30</p>
+            <button onClick={handleLike}>Like</button>
+
             <Comment content= "This is a test comment!"/>
             <Comment content= "This is a  2nd test comment!"/>
             <Comment content= "This is a  3rd test comment!"/>
